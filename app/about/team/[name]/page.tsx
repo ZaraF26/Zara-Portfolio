1e78a977ae308d422cd473ca20f11ab46aac49a0
zafredecpt024 <zafredecpt024@student.wethinkcode.co.za>
2025-09-28 22:33:46 +0200

    Initial commit

---
+++ app/about/team/[name]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const TeamDetailsPage = async (params) => {
+  const name = (await params).name;
+  return <div className='text-black text-5xl'>Team member details page {name}</div>
+};
+
+export default TeamDetailsPage;
