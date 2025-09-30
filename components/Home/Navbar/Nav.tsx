4e199ab25dfe8649398bbcb65fba59a9becbadce
zafredecpt024 <zafredecpt024@student.wethinkcode.co.za>
2025-09-30 15:27:06 +0200

    busy with nav

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,12 +1,33 @@
+"use client";
 import {NavLinks} from '@/constant/constant';
 import React from 'react';
 import { FaCode } from "react-icons/fa";
 import Link from 'next/link';
+import { BiDownload } from 'react-icons/bi';
+import { HiBars3BottomRight } from 'react-icons/hi2';
 
 
 const Nav = () => {
+  
+  const [navBg,setNavBg] = useState("false");
+
+  userEffect(()=> {
+    const handler = ()=> {
+      if(window.scrollY >=90) setNavBg(true);
+      if(window.scrollY < 90)  setNavBg(false)
+
+    };
+
+    window.addEventListener("scroll", handler);
+
+    return()=>window.removeEventListener("scroll", handler);
+
+  },[]);
+
+
+
   return (
-  <div className='transition-all duration-200 h-[12vh] z-[10000] fixed w-full'>
+  <div className={'transition-all ${navBg ? 'bg-[#0f124ed]'} duration-200 h-[12vh] z-[10000] fixed w-full'}>
     <div className='flex items-center h-full justify-between w-[90%] mx-auto'>
         {/* LOGO */}
         <div className='flex items-center space-x-2'>
@@ -23,19 +44,24 @@ const Nav = () => {
            href={link.url} 
            className="text-base hover:text-red-800 text-black font-medium transition-all duration-200" >
                
-                <p>{link.label}</p>
-           </Link>
+                    <p>{link.label}</p>
+              </Link>
+            );
+            })}
+        </div>
+        {/* buttons*/}
+    <div className='flex items-center space-x-4'>
+        {/*CV BUtton*/}
+        <button className='px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-red-800 hover:bg-red-700 transition-all duration-300 text-white flex items-center space-x-2'>
+          <BiDownload className='w-5 h-5'/>
+          <span>Download CV</span>
+        </button>
+        {/* Burger Menu*/}
+        <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-red-800 lg:hidden'/>
+    </div>
+  </div>
+</div>    
         );
-        })}
-    </div>
-    {/* buttons*/}
-<div className='flex items-center space-x-4'>
-    {/*CV BUtton*/}
-    <button className='px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-red-800 hover:bg-red-800'></button>
-</div>
-    </div>
-  </div>    
-    );
-};
+    };
 
 export default Nav;
